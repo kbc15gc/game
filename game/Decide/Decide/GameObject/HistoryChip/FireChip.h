07f90f36e698dd24fe0c7db17d088b49cd3ebb12
sugahara <a>
2017-05-11 16:43:12 +0900

    プレイヤーとチップのあたり判定

--- game/Decide/Decide/GameObject/HistoryChip/FireChip.h
+++ game/Decide/Decide/GameObject/HistoryChip/FireChip.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "fbEngine\_Object\_GameObject\GameObject.h"
 
+class Player;
+
 class FireChip : public GameObject
 {
 public:
 	FireChip(const char* name);
 	void Awake()override;
+	void Start()override;
 	void Update()override;
 private:
+	//プレイヤーオブジェクト
+	Player*		_Player;
 };
