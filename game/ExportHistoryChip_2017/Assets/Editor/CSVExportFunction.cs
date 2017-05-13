673b94daac8d31c7f242d81457f2624169683f6c
flanbel <kbc15b12@stu.kawahara.ac.jp>
2017-05-13 16:20:37 +0900

    NPCExport作成。 UnityでNPCの情報を吐き出せるように。

--- game/ExportHistoryChip_2017/Assets/Editor/CSVExportFunction.cs
+++ game/ExportHistoryChip_2017/Assets/Editor/CSVExportFunction.cs
@@ -4,71 +4,109 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 using System.IO;
+using System;
 
 public class CSVExportFunction : Editor
 {
-    [MenuItem("Export/ExportCSV")]
-    static public void Export()
+    //オブジェクトを出力
+    [MenuItem("Export/ExportObject")]
+    static public void ExportObjects()
     {
         //オブジェクト検索
         GameObject parent = GameObject.Find("ExportObjects");
+        if (parent == null)
+        {
+            Debug.Log("ExportObjectsが見つからなかったのでエクスポートできませんでした。");
+            return;
+        }
         //子供たちのTransformコンポーネントを取得
         Transform[] Children = parent.GetComponentsInChildren<Transform>();
         //ファイルパス
-        string path = Application.dataPath + "/Export/" + SceneManager.GetActiveScene().name + ".csv";
+        string path = Application.dataPath + "/Export/Obj/" + SceneManager.GetActiveScene().name + "Obj" + ".csv";
         //ファイルを開く準備
-        const int size = 9;
         FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
         StreamWriter sw = new StreamWriter(fs);
-        sw.WriteLine("pos,ang,sca");
+        sw.WriteLine("name,pos,ang,sca");
         foreach (Transform child in Children)
         {
             if (child.name == parent.name)
                 continue;
-            float[] output = new float[size];
-            //ポジション
-            output[0] = child.position.x;
-            output[1] = child.position.y;
-            output[2] = child.position.z;
-            //回転
-            output[3] = child.eulerAngles.x;
-            output[4] = child.eulerAngles.y;
-            output[5] = child.eulerAngles.z;
-            //スケール
-            output[6] = child.lossyScale.x;
-            output[7] = child.lossyScale.y;
-            output[8] = child.lossyScale.z;
 
             //名前書き出し
             sw.Write(child.name + ".X");
             sw.Write(',');
-            int num = 0;
-            //書き出す
-            for (short i = 0; i < size; i++)
-            {
-                //値書き出し
-                sw.Write(output[i]);
-                //仕切り文字書き出し
-                if (num < 2)
-                {
-                    num++;
-                    sw.Write('/');
-                }
-                else
-                {
-                    num = 0;
-                    //最後以外
-                    if (i != size - 1)
-                    {
-                        //こんま区切り
-                        sw.Write(',');
-                    }
-                }
-            }
+            //ポジション
+            WriteVector3(sw, child.position);
+            //回転
+            WriteVector3(sw, child.eulerAngles);
+            //スケール
+            WriteVector3(sw, child.lossyScale,false);
+
             //改行
             sw.Write("\r\n");
         }
         sw.Close();
         fs.Close();
+    }
+
+    //オブジェクトを出力
+    [MenuItem("Export/ExportNPC")]
+    static public void ExportNPCs()
+    {
+        //オブジェクト検索
+        GameObject parent = GameObject.Find("ExportNPCs");
+        if (parent == null)
+        {
+            Debug.Log("ExportObjectsが見つからなかったのでエクスポートできませんでした。");
+            return;
+        }
+        //子供たちのTransformコンポーネントを取得
+        Transform[] Children = parent.GetComponentsInChildren<Transform>();
+        //ファイルパス
+        string path = Application.dataPath + "/Export/NPC/" + SceneManager.GetActiveScene().name + "NPC" + ".csv";
+        //ファイルを開く準備
+        FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine("name,pos,ang,sca,messeageid,showtitle");
+        foreach (Transform child in Children)
+        {
+            if (child.name == parent.name)
+                continue;
+
+            //名前書き出し
+            sw.Write(child.name + ".X");
+            sw.Write(',');
+            //ポジション
+            WriteVector3(sw, child.position);
+            //回転
+            WriteVector3(sw, child.eulerAngles);
+            //スケール
+            WriteVector3(sw, child.lossyScale);
+
+            NPC npc = child.GetComponent<NPC>();
+            sw.Write(npc.MesseageID);
+            sw.Write(',');
+
+            sw.Write(Convert.ToInt32(npc.ShowTitle));
+
+            //改行
+            sw.Write("\r\n");
+        }
+        sw.Close();
+        fs.Close();
+    }
+
+    static public void WriteVector3(StreamWriter sw,Vector3 val,bool comma = true)
+    {
+        sw.Write(val.x);
+        sw.Write('/');
+        sw.Write(val.y);
+        sw.Write('/');
+        sw.Write(val.z);
+        //コンマを打つ
+        if (comma)
+        {
+            sw.Write(',');
+        }
     }
 }
