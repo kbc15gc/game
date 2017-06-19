c31334e531194dde1f92a43aed4f6a2e015cb6f8
flanbel <kbc15b12@stu.kawahara.ac.jp>
2017-06-19 11:26:22 +0900

    歴史に関係なくいつもでている「共通オブジェクト」のCommonGroupをExportできるように変更。 CommonGroupを読み込むコードを追加。

--- game/Decide/Decide/GameObject/Village/ContinentObject.cpp
+++ game/Decide/Decide/GameObject/Village/ContinentObject.cpp
@@ -20,4 +20,12 @@ void ContinentObject::LoadModel(const char * filename)
 	SkinModelData* data= new SkinModelData();
 	data->CloneModelData(SkinModelManager::LoadModel(filename), _Anim);
 	_Model->SetModelData(data);
+
+	//当たり判定追加。
+	RigidBody* rigid = AddComponent<RigidBody>();
+	MeshCollider* mesh = AddComponent<MeshCollider>();
+
+	//メッシュコライダー生成。
+	mesh->Create(_Model);
+	rigid->Create(0, mesh, Collision_ID::GROUND);
 }
