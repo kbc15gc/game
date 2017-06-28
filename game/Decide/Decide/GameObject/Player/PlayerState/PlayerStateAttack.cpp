f99663e6eed3342f37599a80b965e8a59fa714ed
sugahara <a>
2017-06-28 15:50:00 +0900

    あたり判定追加。 家。策

--- game/Decide/Decide/GameObject/Player/PlayerState/PlayerStateAttack.cpp
+++ game/Decide/Decide/GameObject/Player/PlayerState/PlayerStateAttack.cpp
@@ -117,6 +117,6 @@ void PlayerStateAttack::Attack(AttackCollisionParameter pram)
 		_SE->Play(false);
 		//UŒ‚ƒRƒŠƒWƒ‡ƒ“ì¬
 		AttackCollision* attack = INSTANCE(GameObjectManager)->AddNew<AttackCollision>("attack01", 1);
-		attack->Create(_Player->_PlayerParam->GiveDamageMass(),pram.pos, pram.rot, pram.scale, AttackCollision::CollisionMaster::Player, pram.lifetime, _Player->transform);
+		attack->Create(_Player->_PlayerParam->GiveDamageMass(),pram.pos, pram.rot, pram.scale, AttackCollision::CollisionMaster::Player, pram.lifetime,0.0f, _Player->transform);
 	}
 }
