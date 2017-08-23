ae30a2dd18008e1d6eb55c780aaa9e4e29b5f7f6
NoborioYoshiki <a>
2017-08-23 14:38:49 +0900

    プレイヤーの所持欄に追加するアイテムの種類を見て適切にアイテムを追加する処理を追加

--- game/Decide/Decide/GameObject/Village/ItemManager.cpp
+++ game/Decide/Decide/GameObject/Village/ItemManager.cpp
@@ -21,18 +21,93 @@ void ItemManager::LoadAllItemData()
 
 		switch (i)
 		{
-		case 1:
+		case 0:
 			//ファイルからアイテム情報読み込み。
 			Support::LoadCSVData<Item::ItemInfo>(filepath, Item::Itemdata, ARRAY_SIZE(Item::Itemdata), _ItemListVec);
 			break;
-		case 2:
+		case 1:
 			//ファイルから武器情報読み込み。
 			Support::LoadCSVData<Weapon::WeaponInfo>(filepath, Weapon::Weapondata, ARRAY_SIZE(Weapon::Weapondata), _WeaponList);
 			break;
-		case 3:
+		case 2:
 			//ファイルから防具情報読み込み。
 			Support::LoadCSVData<Armor::ArmorInfo>(filepath, Armor::Armordata, ARRAY_SIZE(Armor::Armordata), _ArmorList);
 			break;
 		}
 	}
+	AddPlayerItemList(ItemKodeE::Item, &_ItemListVec[0]->ID, _ItemListVec[0]->Name, _ItemListVec[0]->Description, &_ItemListVec[0]->Value,  nullptr, nullptr);
+	AddPlayerItemList(ItemKodeE::Item, &_ItemListVec[1]->ID, _ItemListVec[1]->Name, _ItemListVec[1]->Description, &_ItemListVec[1]->Value,  nullptr, nullptr);
+	AddPlayerItemList(ItemKodeE::Armor, &_ArmorList[0]->ID, _ArmorList[0]->Name, _ArmorList[0]->Description, &_ArmorList[0]->Value, &_ArmorList[0]->ATK, &_ArmorList[0]->DEF);
+
 }
+
+void ItemManager::AddPlayerItemList(ItemKodeE kode, int* id, const char* name, const char* des, int* value,  int* atk, int* def)
+{
+	switch (kode)
+	{
+	case ItemManager::ItemKodeE::Item:
+		if (ItemCounter < ListNum)
+		{
+			_PlayerItemList[ItemCounter].ID = *id;
+			strcpy(_PlayerItemList[ItemCounter].Name, name);
+			strcpy(_PlayerItemList[ItemCounter].Description, des);
+			_PlayerItemList[ItemCounter].Value = *value;
+			ItemCounter++;
+			break;
+		}
+		else
+		{
+			//char error[256];
+			//sprintf(error, "プレイヤーのアイテムリストにアイテムを追加できませんでした。");
+			//MessageBoxA(0, error, "アイテムの追加失敗", MB_ICONWARNING);
+			break;
+		}
+	case ItemManager::ItemKodeE::Armor:
+		if (ArmorCounter < ListNum)
+		{
+			_PlayerArmorList[ArmorCounter].ID = *id;
+			strcpy(_PlayerArmorList[ArmorCounter].Name, name);
+			strcpy(_PlayerArmorList[ArmorCounter].Description, des);
+			_PlayerArmorList[ItemCounter].Value = *value;
+			_PlayerArmorList[ArmorCounter].ATK = *atk;
+			_PlayerArmorList[ArmorCounter].DEF = *def;
+			ArmorCounter++;
+			break;
+		}
+		else
+		{
+			//char error[256];
+			//sprintf(error, "プレイヤーのアイテムリストにアイテムを追加できませんでした。");
+			//MessageBoxA(0, error, "アイテムの追加失敗", MB_ICONWARNING);
+			break;
+		}
+	case ItemManager::ItemKodeE::Weapon:
+		if (WeapoCounter < ListNum)
+		{
+			_PlayerWeaponList[WeapoCounter].ID = *id;
+			strcpy(_PlayerWeaponList[WeapoCounter].Name, name);
+			strcpy(_PlayerWeaponList[WeapoCounter].Description, des);
+			_PlayerWeaponList[WeapoCounter].Value = *value;
+			_PlayerWeaponList[WeapoCounter].ATK = *atk;
+			WeapoCounter++;
+			break;
+		}
+		else
+		{
+			//char error[256];
+			//sprintf(error, "プレイヤーのアイテムリストにアイテムを追加できませんでした。");
+			//MessageBoxA(0, error, "アイテムの追加失敗", MB_ICONWARNING);
+			break;
+		}
+	default:
+		char error[256];
+		sprintf(error, "指定したアイテムコードが無効です。");
+		MessageBoxA(0, error, "アイテムコードの指定を失敗", MB_ICONWARNING);
+		break;
+	}
+}
+
+//Armor::ArmorInfo *ItemManager::GetPlayerAromorInfo()
+//{
+//	return &_PlayerArmorList[0];
+//}
--- game/Decide/Decide/GameObject/Village/ItemManager.h
+++ game/Decide/Decide/GameObject/Village/ItemManager.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include<tuple>
 namespace Item
 {
 	//アイテムの情報をまとめた構造体。
@@ -34,7 +34,7 @@ namespace Armor
 		int DEF;				//防具を装備した時に上がる防御力。
 	};
 
-	//メンバ変数の情報設定
+	//メンバ変数の情報設定。
 	static Support::DATARECORD Armordata[] =
 	{
 		{ "ID",Support::DataTypeE::INT ,			offsetof(struct ArmorInfo,ID),			sizeof(int) },
@@ -59,7 +59,7 @@ namespace Weapon
 		int ATK;				//武器を装備した時に上がる攻撃力。
 	};
 
-	//メンバ変数の情報設定
+	//メンバ変数の情報設定。
 	static Support::DATARECORD  Weapondata[] =
 	{
 		{ "ID",Support::DataTypeE::INT ,			offsetof(struct WeaponInfo,ID),			sizeof(int) },
@@ -70,22 +70,22 @@ namespace Weapon
 	};
 }
 
-//アイテムの情報を管理するクラス
+//アイテムの情報を管理するクラス。
 class ItemManager
 {
 public:
 	ItemManager();
 
+	enum class ItemKodeE
+	{
+		Invalid = -1,	//無効。
+		Item = 0,		//アイテム。
+		Armor = 1,		//防具。
+		Weapon = 2,		//武器。
+	};
+
+	//アイテム、武器、防具を一括で読み込み。
 	void LoadAllItemData();
-
-	//アイテムの情報を読み込み。
-	//void LoadItemData();
-
-	//防具の情報を読み込み。
-	//void LoadArmorData();
-
-	//武器の情報を読み込み。
-	//void LoadWeaponData();
 
 	Item::ItemInfo* GetItem(const unsigned int& id,const unsigned int& type)
 	{
@@ -106,18 +106,44 @@ public:
 		}
 		return _Instance;
 	}
+
+
+	//プレイヤーの所持品にアイテムを追加。第1引数:追加するアイテムの種類、第2引数:アイテムID、第3引数:名前、第4引数:アイテムの説明、第5引数:値段、、第6引数:攻撃力、第7引数:防御力。
+	void AddPlayerItemList(ItemKodeE kode, int* id = nullptr, const char* name = nullptr, const char* des = nullptr, int* value = nullptr, int* atk = nullptr, int* def = nullptr);
+
+	//プレイヤーが所持している防具のリストを取得。
+	//Armor::ArmorInfo *GetPlayerAromorInfo();
+
+
 private:
-	//アイテムのリスト
+	//アイテムのリスト。
 	vector<unique_ptr<Item::ItemInfo>> _ItemList[3];
 
-	//アイテムのリスト
+	//アイテムのリスト。
 	vector<unique_ptr<Item::ItemInfo>> _ItemListVec;
 
-	//防具のリスト
+	//防具のリスト。
 	vector<unique_ptr<Armor::ArmorInfo>> _ArmorList;
 
-	//武器のリスト
+	//武器のリスト。
 	vector<unique_ptr<Weapon::WeaponInfo>> _WeaponList;
+
+	//プレイヤーが所持しているアイテムのリスト。
+	Item::ItemInfo _PlayerItemList[5];
+
+	//プレイヤーが所持している防具のリスト。
+	Armor::ArmorInfo _PlayerArmorList[5];
+
+	Armor::ArmorInfo arm[2];
+
+	//プレイヤーが所持している武器のリスト。
+	Weapon::WeaponInfo _PlayerWeaponList[5];
+
+	//所持品を格納するリストの最大数。
+	int ListNum = 5;
+
+	//プレイヤーが持っているアイテム、防具、武器の数。
+	int ItemCounter, ArmorCounter, WeapoCounter = 0;
 	
 	static ItemManager* _Instance;
 };
