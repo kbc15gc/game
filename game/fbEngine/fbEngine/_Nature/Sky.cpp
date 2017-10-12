e2d5c4ef71a0694c991b315f95f96bf05bcb4fb0
TadashiHiramatsu <hiramatsutadashi0609@gmail.com>
2017-10-12 20:05:58 +0900

    /

--- game/fbEngine/fbEngine/_Nature/Sky.cpp
+++ game/fbEngine/fbEngine/_Nature/Sky.cpp
@@ -45,7 +45,7 @@ void Sky::Update()
 	Camera* camera = INSTANCE(GameObjectManager)->mainCamera;
 	if (camera != nullptr)
 	{
-		const float TMP = 10.0f;
+		const float TMP = 1.0f;
 		//‘¾—z‚ÌŠp“x‚ð‰ÁŽZ.
 		_SunAngle += 0.02f * Time::DeltaTime() * TMP;
 
