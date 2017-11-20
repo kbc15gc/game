fff3c2e892414eb32a55a530d2b0eea3d9ecda20
TadashiHiramatsu <hiramatsutadashi0609@gmail.com>
2017-11-20 17:06:02 +0900

    ・歴史書にて選択していない場所に差し込まれているページは色が変わるように設定

--- game/Decide/Decide/GameObject/History/HistoryBook/HistoryBook.h
+++ game/Decide/Decide/GameObject/History/HistoryBook/HistoryBook.h
@@ -257,6 +257,25 @@ public:
 		return false;
 	}
 
+	void SetNowSelectLocation(int value)
+	{
+		_NowSelectLocation = value;
+		for (int i = 0; i < _HistoryPageList.size(); i++)
+		{
+			for (auto itPage : _HistoryPageList[i])
+			{
+				if (i == _NowSelectLocation)
+				{
+					itPage->SetBlendColor(Color(1.0f, 1.0f, 1.0f));
+				}
+				else
+				{
+					itPage->SetBlendColor(Color(0.2f, 0.2f, 0.2f));
+				}
+			}
+		}
+	}
+
 private:
 
 	/**
@@ -321,6 +340,9 @@ private:
 
 	/** ページクラスのポインタ. */
 	vector<vector<HistoryPage*>> _HistoryPageList;
+
+	/** 現在みている場所. */
+	int _NowSelectLocation = 0;
 
 	/** 操作可能フラグ. */
 	bool _IsOperation = true;
