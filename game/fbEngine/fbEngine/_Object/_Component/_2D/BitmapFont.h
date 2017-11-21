7564031be3ef3540a879881210661a5ed1ab9896
flanbel <kbc15b12@stu.kawahara.ac.jp>
2017-11-21 15:59:09 +0900

    [update]BitmapFontの機能追加。

--- game/fbEngine/fbEngine/_Object/_Component/_2D/BitmapFont.h
+++ game/fbEngine/fbEngine/_Object/_Component/_2D/BitmapFont.h
@@ -8,7 +8,24 @@ class BitmapFont :public Component
 public:
 	BitmapFont(GameObject* g, Transform* t);
 	void Start()override;
+	void Update()override;
+	void Render()override;
+
+	void LoadFont(string filename);
+	void SetText(string text);
 private:
-	//頂点
-	static Vertex* _Vertex;
+	//エフェクト
+	Effect* _Effect;
+	//ビットマップフォントのテクスチャ。
+	TEXTURE* _Bitmap;
+	//文字列。
+	string _Text;
+	//テキストサイズ。
+	Vector2 _TextSize;
+
+	//
+	LPDIRECT3DVERTEXBUFFER9 _PosBuffer;
+	LPDIRECT3DVERTEXBUFFER9 _UVBuffer;
+	//デコレーション
+	LPDIRECT3DVERTEXDECLARATION9 _Declaration;	
 };
