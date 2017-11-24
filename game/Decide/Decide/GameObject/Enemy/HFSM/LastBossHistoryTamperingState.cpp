6faea379c077c5e4c70dc6930b190b32b5f02a8e
ShoOkawa <a>
2017-11-24 10:43:16 +0900

    魔王城作成中

--- game/Decide/Decide/GameObject/Enemy/HFSM/LastBossHistoryTamperingState.cpp
+++ game/Decide/Decide/GameObject/Enemy/HFSM/LastBossHistoryTamperingState.cpp
@@ -9,16 +9,21 @@
 
 LastBossHistoryTamperingState::LastBossHistoryTamperingState(EnemyCharacter* Object) : EnemyState(Object)
 {
-	//_buildingsParent.reset(new Transform(nullptr,nullptr));
-	//_buildingsParent->SetPosition(_EnemyObject->transform->GetPosition());
-	//_buildingsParent->SetRotation(Quaternion::Identity);
-	// INSTANCE(HistoryManager)->CreateBuilding("Asset/Data/LastBossStageEquipment/Test.csv", _historyBuildings);
-	// for (auto obj : _historyBuildings) {
-	//	 // 吐き出した座標をバトルフィールドの座標系に変換。
-	//	 obj->transform->SetParent(_buildingsParent.get());
-	//	 obj->transform->SetLocalPosition(obj->transform->GetPosition());
-	//	 obj->transform->SetLocalRotation(obj->transform->GetRotation());
-	// }
+	_buildingsParent.reset(new Transform(nullptr,nullptr));
+	_buildingsParent->SetPosition(_EnemyObject->transform->GetPosition() + Vector3(0.0f,0.0f,-10.0f));
+	Quaternion rot;
+	rot = Quaternion::Identity;
+	rot.SetRotation(Vector3::axisX, D3DXToRadian(180.0f));
+	_buildingsParent->SetRotation(Quaternion::Identity);
+	 INSTANCE(HistoryManager)->CreateBuilding("Asset/Data/LastBossStageEquipment/LastDungeonObj.csv", _historyBuildings);
+	 for (auto obj : _historyBuildings) {
+		 // 吐き出した座標をバトルフィールドの座標系に変換。
+		 obj->transform->SetParent(_buildingsParent.get());
+		 obj->transform->SetLocalPosition(obj->transform->GetPosition());
+		 Quaternion work = obj->transform->GetRotation();
+		 work.Multiply(rot);
+		 obj->transform->SetLocalRotation(work);
+	 }
 }
 
 
@@ -47,13 +52,6 @@ void LastBossHistoryTamperingState::_UpdateSubClass() {
 	// 大魔法がキャンセルされるとダウンに移行。
 	_EnemyObject->ChangeStateRequest(static_cast<EnemyCharacter::State>(LastBoss::LastBossState::LastBossDown));
 	
-	for (auto obj : _historyBuildings) {
-		// 吐き出した座標をバトルフィールドの座標系に変換。
-		if (static_cast<ContinentObject*>(obj)->GetIsAddPhysicsWorld()) {
-			OutputDebugString("LastBossBuild!");
-		}
-	}
-
 	// ※暫定処理。
 	//_EndState();
 }
